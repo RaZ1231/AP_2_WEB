4f78270ce9a85c9bc629cf7ede4f83b633acff21
Raz <raz.s121@gmail.com>
2017-06-13 18:45:42 +0300

    basic single player and moving

--- AP2_WEB/View/Maze.js
+++ AP2_WEB/View/Maze.js
@@ -1,93 +1,141 @@
-﻿function drawMaze(maze) {
-    var myCanvas = document.getElementById("mazeCanvas");
-    var context = mazeCanvas.getContext("2d");
-    var rows = maze.length; var cols = maze[0].length;
-    var cellWidth = mazeCanvas.width / cols;
-    var cellHeight = mazeCanvas.height / rows;
-    var x = 0;
-    var y = 0;
+﻿var context;
+var cellWidth;
+var cellHeight;
+var maze;
+var pRow;
+var pCol;
+var playerImg;
 
-    for (x = 0; x < rows; x++) {
-        for (y = 0; y < cols; y++) {
-            if (maze[x][y] == '*') {
-                break;
+(function ($) {
+    $.fn.mazeBoard = function (
+        mazeData,
+        startRow, startCol,
+        exitRow, exitCol,
+        playerImage,
+        exitImage,
+        enabled,
+        moveFunction
+    ) {
+
+        var myCanvas = $(this);
+        context = mazeCanvas.getContext("2d");
+        var rows = mazeData.length; var cols = mazeData[0].length;
+        cellWidth = mazeCanvas.width / cols;
+        cellHeight = mazeCanvas.height / rows;
+        maze = mazeData;
+        var x = 0;
+        var y = 0;
+        pRow = startRow;
+        pCol = startCol;
+
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                if (mazeData[i][j] == 1) {
+                    context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+                }
             }
         }
-    }
 
-    var enterence = document.createElement("enterence");
-    enterence.src = "/Resources/door.jpg";
+        var exit = exitImage;
+        context.drawImage(exit, exitCol * cellWidth, exitRow * cellHeight, cellWidth, cellHeight);
 
-    var exit = new Image();
-    exit.src = "/Resources/Spiral.jpg";
-    exit.height = mazeCanvas.height / rows;
-    exit.width = mazeCanvas.width / cols;
-    exit.onload = function () {
-        context.drawImage(exit, x*cellWidth, y*cellHeight, exit.width, exit.height);
-    }
-    
-    var player = document.getElementById("player");
-    player.onload = function () {
-        ply_wid = mazeCanvas.width / cols;
-        ply_hei = mazeCanvas.height / rows;
+        var player = playerImage;
+        
+        playerImg = new Image();
+        playerImg.src = "/Resources/mario.jpg";
+        playerImg.style.position = absolute;
+        var topPX = startRow * cellHeight + 'px';
+        var leftPX = startCol * cellWidth + 'px';
+        playerImg.style.top = topPX;
+        playerImg.style.left = leftPX;
+        context.drawImage(player, startCol * cellWidth, startRow * cellHeight, cellWidth, cellHeight);
 
-        context.drawImage(player, 0, 0, ply_wid, ply_hei);
-    }
+        var enterence = document.getElementById("enterence");
+        enterence.src = "/Resources/door.jpg";
 
+        return this;
+    };
+})(jQuery);
 
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < cols; j++) {
-            if (maze[i][j] == 1) {
-                context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
-            }
-        }
-    }
-}
 
 function keydown(e) {
-    var myCanvas = document.getElementById("mazeCanvas");
-    var context = mazeCanvas.getContext("2d");
-    var rows = maze.length; var cols = maze[0].length;
-    var cellWidth = mazeCanvas.width / cols;
-    var cellHeight = mazeCanvas.height / rows;
-    var player = document.getElementById("player");
-    var ply_wid = mazeCanvas.width / cols;
-    var ply_hei = mazeCanvas.height / rows;
-
     switch (e.which) {
         case 37:
-            alert("left");
+            move("left", pRow, pCol);
             break;
         case 38:
-            alert("up");
+            move("up", pRow, pCol);
             break;
         case 39:
-            alert("right");
-            context.drawImage(player, 0, 5 * cellHeight, ply_wid, ply_hei);
-
+            move("right", pRow, pCol);
+            alert("woohoo");
             break;
         case 40:
-            alert("down");
+            move("down", pRow, pCol);
             break;
     }
 }
 
-function handleKeys() {
-    var input = document.getElementById("input");
-    input.onkeydown(function (e) {
-        switch (e.which) {
-            case 37:
-                alert("left");
-                break;
-            case 38:
-                alert("up");
-                break;
-            case 39:
-                alert("right");
-                break;
-            case 40:
-                alert("down");
-                break;
-        }
-    });
+function move(direction, playerRow, playerCol) {
+    player.hide();
+    switch (direction) {
+        case "left":
+            if (playerCol > 0) {
+                if (mazeData[playerRow][playerCol - 1] != '1') {
+                    //playerCol = playerCol - 1;
+                    var element = document.getElementById("player");
+                    element.style.left = parseInt(element.style.left) - cellWidth + 'px';
+                }
+            }
+            break;
+        case "up":
+            if (playerRow > 0) {
+                if (mazeData[playerRow - 1][playerCol] != '1') {
+                    //playerRow = playerRow - 1;
+                    var element = document.getElementById("player");
+                    element.style.top = parseInt(element.style.top) - cellHeight + 'px';
+                }
+            }
+            break;
+        case "right":
+            if (playerCol < mazeData[0].length) {
+                if (mazeData[playerRow][playerCol + 1] != '1') {
+                    //playerCol = playerCol + 1;
+                    var element = document.getElementById("player");
+                    element.style.left = parseInt(element.style.left) + cellWidth + 'px';
+                    alert("woohoo");
+                }
+            }
+            break;
+        case "down":
+            if (playerCol < mazeData[0].length) {
+                if (mazeData[playerRow + 1][playerCol] != '1') {
+                    //playerRow = playerRow + 1;
+                    var element = document.getElementById("player");
+                    element.style.top = parseInt(element.style.top) + cellHeight + 'px';
+                }
+            }
+            break;
+    }
+    //context.drawImage(player, playerCol * cellHeight, playerRow * cellWidth, cellWidth, cellHeight);
 }
+
+var mazeData = [[0, 1, 0, 1, 0, 1],
+['#', 0, 0, 1, 1, 0],
+[0, 1, 0, 0, 0, 0],
+[0, 1, 0, 1, 1, 1],
+[0, 1, 0, 0, 0, 0],
+[1, 1, 0, '*', 1, 0],
+[0, 0, 0, 0, 0, 1]];
+
+var startRow = 1;
+var startCol = 0;
+var exitRow = 5;
+var exitCol = 3
+var playerImage = document.getElementById("player");
+var exitImage = document.getElementById("exit");
+var enabled = true;
+var moveFunction = undefined;
+
+
+
