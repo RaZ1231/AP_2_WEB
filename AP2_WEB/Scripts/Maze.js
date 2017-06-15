bc7d7d3eab1fa0ebb981c4324b3a6045c2aefd1d
Elisheva Broyer <elish214@gmail.com>
2017-06-15 20:34:44 +0300

    server-client integration has began!

--- AP2_WEB/Scripts/Maze.js
+++ AP2_WEB/Scripts/Maze.js
@@ -7,6 +7,7 @@ var context;
 var cellWidth;
 var cellHeight;
 var maze;
+var mazeMatrix;
 var playerProp;
 var myCanvas;
 var exitR;
@@ -14,88 +15,116 @@ var exitC;
 var startR;
 var startC;
 
-(function ($) {
-    $.fn.mazeBoard = function (
-        mazeData,
-        startRow, startCol,
-        exitRow, exitCol,
-        playerImage,
-        exitImage,
-        enabled,
-        moveFunction
-    ) {
+$(document).ready(function () {
 
-        myCanvas = document.getElementById("mazeCanvas");
-        context = myCanvas.getContext("2d");
-        var rows = mazeData.length;
-        var cols = mazeData[0].length;
-        cellWidth = (myCanvas.width) / cols;
-        cellHeight = (myCanvas.height) / rows;
-        exitR = exitRow;
-        exitC = exitCol;
-        startR = startRow;
-        startC = startCol;
+    // generate
+    $("#btnNewGame").click(function () {
+        var genRequest = {
+            Name: $("#mazeName").val(),
+            Rows: $("#rows").val(),
+            Cols: $("#cols").val()
+        }
+        $.getJSON("/Maze/generate" + "/" + genRequest.Name + "/" + genRequest.Rows + "/" + genRequest.Cols)
+            .done(function (jsMaze) {
+                maze = jsMaze;
+                $("#product").text(maze.Name + " : " + maze.Maze);
+                $("#mazeCanvas").mazeBoard(
+       maze.Maze, // the matrix containing the maze cells
+       maze.Start.Row,
+       maze.Start.Col, // initial position of the player
+       maze.End.Row,
+       maze.End.Col, // the exit position 
+       document.getElementById("player"), // player's icon (of type Image)
+       document.getElementById("exit"), // exit icon
+       true,
+       undefined
+        );
+            })
+    });
 
-        maze = mazeData;
+    (function ($) {
+        $.fn.mazeBoard = function (
+            mazeData,
+            startRow, startCol,
+            exitRow, exitCol,
+            playerImage,
+            exitImage,
+            enabled,
+            moveFunction
+        ) {
 
-        playerProp = new playerObj();
-        playerProp.pRow = startRow;
-        playerProp.pCol = startCol;
+            myCanvas = document.getElementById("mazeCanvas");
+            context = myCanvas.getContext("2d");
+            var rows = maze.Rows;
+            var cols = maze.Cols;
+            cellWidth = (myCanvas.width) / cols;
+            cellHeight = (myCanvas.height) / rows;
+            exitR = exitRow;
+            exitC = exitCol;
+            startR = startRow;
+            startC = startCol;      
 
-        for (var i = 0; i < rows; i++) {
-            for (var j = 0; j < cols; j++) {
-                if (maze[i][j] == 1) {
-                    context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+            playerProp = new playerObj();
+            playerProp.pRow = startRow;
+            playerProp.pCol = startCol;
+
+
+
+            for (var i = 0; i < rows; i++) {
+                for (var j = 0; j < cols; j++) {
+                    if (mazeMatrix[i][j] == 1) {
+                        context.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+                    }
                 }
             }
-        }
 
-        var player = playerImage;
-        context.drawImage(player, startCol * cellWidth, startRow * cellHeight, cellWidth, cellHeight);
+            var player = playerImage;
+            context.drawImage(player, startCol * cellWidth, startRow * cellHeight, cellWidth, cellHeight);
 
-        var exit = exitImage;
-        context.drawImage(exit, exitCol * cellWidth, exitRow * cellHeight, cellWidth, cellHeight);
+            var exit = exitImage;
+            context.drawImage(exit, exitCol * cellWidth, exitRow * cellHeight, cellWidth, cellHeight);
 
-        
-        return this;
-    };
-})(jQuery);
+            return this;
+        };
+    })(jQuery);
 
+    document.addEventListener("keydown", keydown, false);
+});
 
 function keydown(e) {
-    if(playerProp.pRow != exitR || playerProp.pCol != exitC) {
+    if (playerProp.pRow != exitR || playerProp.pCol != exitC) {
         context.fillStyle = "white";
         context.fillRect(playerProp.pCol * cellHeight, playerProp.pRow * cellWidth, cellWidth, cellHeight);
     } else {
         var exit = document.getElementById("exit");
         context.drawImage(exit, exitC * cellWidth, exitR * cellHeight, cellWidth, cellHeight);
     }
-    
+
     switch (e.which) {
         case 37: // left
             if (playerProp.pCol > 0) {
-                if (maze[playerProp.pRow][playerProp.pCol - 1] != 1) {
+                if (mazeMatrix[playerProp.pRow][playerProp.pCol - 1] != 1) {
                     playerProp.pCol = playerProp.pCol - 1;
                 }
             }
             break;
         case 38: //up
             if (playerProp.pRow > 0) {
-                if (maze[playerProp.pRow - 1][playerProp.pCol] != 1) {
+                if (mazeMatrix[playerProp.pRow - 1][playerProp.pCol] != 1) {
                     playerProp.pRow = playerProp.pRow - 1;
                 }
             }
             break;
         case 39: // right
             if (playerProp.pCol < maze[0].length - 1) {
-                if (maze[playerProp.pRow][playerProp.pCol + 1] != 1) {
+                if (mazeMatrix[playerProp.pRow][playerProp.pCol + 1] != 1) {
                     playerProp.pCol = playerProp.pCol + 1;
                 }
             }
             break;
         case 40: // down
             if (playerProp.pRow < maze[0].length - 1) {
-                if (maze[playerProp.pRow + 1][playerProp.pCol] != 1) {
+                if (mazeMatrix[playerProp.pRow + 1][playerProp.pCol] != 1) {
                     playerProp.pRow = playerProp.pRow + 1;
                 }
             }
@@ -118,14 +147,13 @@ function solve(solution) {
 
     var sol = "11333333331";
     solution = sol;
-    var solveObj = {solution: solution, i:0,intervalID:null};
-    solveObj.intervalID = setInterval(solveCallback, 1000 , solveObj);
+    var solveObj = { solution: solution, i: 0, intervalID: null };
+    solveObj.intervalID = setInterval(solveCallback, 1000, solveObj);
 }
-
 
 function solveCallback(obj) {
     var i = (obj.i)++;
-    if(i >= obj.solution.length){
+    if (i >= obj.solution.length) {
         clearInterval(obj.intervalID);
         return;
     }
@@ -140,37 +168,39 @@ function solveCallback(obj) {
     switch (obj.solution[i]) {
         case '0': // left
             if (playerProp.pCol > 0) {
-                if (maze[playerProp.pRow][playerProp.pCol - 1] != 1) {
+                if (mazeMatrix[playerProp.pRow][playerProp.pCol - 1] != 1) {
                     playerProp.pCol = playerProp.pCol - 1;
                 }
             }
             break;
         case '2': //up
             if (playerProp.pRow > 0) {
-                if (maze[playerProp.pRow - 1][playerProp.pCol] != 1) {
+                if (mazeMatrix[playerProp.pRow - 1][playerProp.pCol] != 1) {
                     playerProp.pRow = playerProp.pRow - 1;
                 }
             }
             break;
         case '1': // right
-            if (playerProp.pCol < maze[0].length - 1) {
-                if (maze[playerProp.pRow][playerProp.pCol + 1] != 1) {
+            if (playerProp.pCol < mazeMatrix[0].length - 1) {
+                if (mazeMatrix[playerProp.pRow][playerProp.pCol + 1] != 1) {
                     playerProp.pCol = playerProp.pCol + 1;
                 }
             }
             break;
         case '3': // down
-            if (playerProp.pRow < maze[0].length) {
-                if (maze[playerProp.pRow + 1][playerProp.pCol] != 1) {
+            if (playerProp.pRow < mazeMatrix[0].length) {
+                if (mazeMatrix[playerProp.pRow + 1][playerProp.pCol] != 1) {
                     playerProp.pRow = playerProp.pRow + 1;
                 }
             }
             break;
     }
-	
+
     context.drawImage(player, playerProp.pCol * cellHeight, playerProp.pRow * cellWidth, cellWidth, cellHeight);
 
     if (playerProp.pRow == exitR && playerProp.pCol == exitC) {
         alert("Yay!");
-    }   
+    }
 }
+
+
