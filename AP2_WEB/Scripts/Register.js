f20b5950ee438e54d1fe899fad89d876d9024cef
Raz <raz.s121@gmail.com>
2017-06-19 22:49:45 +0300

    mid register

--- AP2_WEB/Scripts/Register.js
+++ AP2_WEB/Scripts/Register.js
@@ -4,4 +4,39 @@
     } else {
         $("#menuBar").load("ConnectedMenu.html");
     }
+
+    var apiUrl = "/api/Users/";
+
+    // register
+    $("#btnSubmit").click(function () {
+        var user = {
+            Username: $("#inputUsername").val(),
+            Password: $("#inputPassword").val(),
+            Email: $("#inputEmail").val(),
+            JoinDate: new Date(),
+            Wins: 0,
+            Loses: 0
+        }
+
+        $.ajax({
+            url: apiUrl,
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(user),
+            success: function (result) {
+                $("#txtAnswer").text("User added! click List btn to see changes");
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                $("#txtAnswer").text("ERROR");
+                switch (XMLHttpRequest.responseJSON) {
+                    case "Username":
+                        $("#txtAnswer").text("Username taken!");
+                        break;
+                    case "Email":
+                        $("#txtAnswer").text("Email taken!");
+                        break;
+                }
+            }
+        });
+    });
 });
